4ad283c1a8ebe5c5a807b6e38f4f1b4d3268c9f5
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-06-19 17:58:42 -0700

    создали класс Group и его конструктор для переметров name, header, footer в отдельном файле group.py. В файле test_add_group применили параметры конструктора в соотвтетствующие методы.

--- test_add_group.py
+++ test_add_group.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from group import Group
 from selenium import webdriver
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.keys import Keys
@@ -20,7 +21,7 @@ class TestAddGroup(unittest.TestCase):  # обязательное указан�
         self.login(wd, username="admin", password="secret")
 
         self.open_group_page(wd)
-        self.create_group(wd, name="First", header="beginning", footer="finishing")
+        self.create_group(wd, Group(name="First", header="beginning", footer="finishing")) # создаем объект класса с конструктором по умолчанию
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -35,19 +36,19 @@ class TestAddGroup(unittest.TestCase):  # обязательное указан�
     def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -83,7 +84,7 @@ class TestAddGroup(unittest.TestCase):  # обязательное указан�
         self.login(wd, username="admin", password="secret")
 
         self.open_group_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
     
