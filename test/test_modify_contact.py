d576fd3f18b976ce48ef7c2f5faf529541fca260
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-07-07 20:03:52 -0700

    Организованы проверки списков в тестах: добавления, удаления, модификации групп и контактов. В контактах осуществляются проверки только по имени, фамилии и id.
    Выполнение задания № 11.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,30 +6,48 @@ def test_modify_contact_address(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test"))
     old_contacts = app.contact.get_contact_list()
-    modified_contact = Contact(name="Anna", lastname="D", address="Address edited")
+    modified_contact = Contact(address="Address edited")
     modified_contact.id = old_contacts[0].id
     app.contact.modify_contact(modified_contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[0] = modified_contact
+    #old_contacts[0] = modified_contact  ?? в отличие от теста удаления контакта эта строчка не нужна, тк ИМЯ контакта НЕ менял!!??
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_contact_phone(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test"))
-    app.contact.modify_contact(Contact(mobile="Cell phone number edited"))
+    old_contacts = app.contact.get_contact_list()
+    modified_contact = Contact(mobile="Cell phone number edited")
+    app.contact.modify_contact(modified_contact)
+    modified_contact.id = old_contacts[0].id
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_contact_email(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test"))
-    app.contact.modify_contact(Contact(email="email edited"))
+    old_contacts = app.contact.get_contact_list()
+    modified_contact = Contact(email="email edited")
+    app.contact.modify_contact(modified_contact)
+    modified_contact.id = old_contacts[0].id
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
 def test_modify_contact_landline(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test"))
-    app.contact.modify_contact(Contact(landline="Landline edited"))
+    old_contacts = app.contact.get_contact_list()
+    modified_contact = Contact(landline="Landline edited")
+    app.contact.modify_contact(modified_contact)
+    modified_contact.id = old_contacts[0].id
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -21,6 +21,10 @@ def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="header edited"))
+    modified_group = Group(header="header edited")
+    modified_group.id = old_groups[0].id
+    app.group.modify_first_group(modified_group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    #old_groups[0] = modified_group  # ??? не нужно присвоение, потому что зд ИМЯ группы НЕ менял!!  ???
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
