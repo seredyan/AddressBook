aa2aca701b41d9c2114cfb8e8f5719c3e66ef540
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-07-14 19:14:18 -0700

    Сделал 1ю часть задания № 14 (Реализовать тест, который проверяет, что для некоторого одного случайно выбранного контакта информация на главной странице (в таблице) соответствует информации, представленной в форме редактирования контакта (где задаются все его свойства), при этом проверяться должны все представленные свойства -- имя, фамилия, адрес, телефоны, адреса электронной почты)
    Подправил в тесте удаления контакта название метода app.contact.get_contact_list() на более последний вариант app.contact.get_contact_list_split()

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -7,12 +7,12 @@ from random import randrange
 def test_delete_some_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test"))
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = app.contact.get_contact_list_split()
     index = randrange(len(old_contacts))
     app.contact.delete_some_contact(index)
 
     assert len(old_contacts) - 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = app.contact.get_contact_list_split()
     old_contacts[index:index+1] = []
     #old_contacts[0:1] = []
     # old_contacts.pop(0)
