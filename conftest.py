9c85ca788b200efddb00b294e5877d44bc06e174
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-07-21 12:04:02 -0700

    Повторил действия из ролика 6_1:
    - добавил файл конфигурации target.json где основные данные (логин, пароль итп) теперь их легче модифицировать чтобы вызывать разные тесты
    
    - объединил (упростил код)  в одну строчку: if fixture is None or not fixture.is_valid()
    
    - сделал edit configuration в pycharm указав путь тестов к всеобщей папке проекта, чтобы питон смог прочитать файл target.json
    
    !! Есть проблема c test_phones. Он не проходит если разные варианты конфигурации. Доработать согласно советам Алексея в дом задании № 14

--- conftest.py
+++ conftest.py
@@ -1,22 +1,24 @@
 
 import pytest
+import json
 from fixture.application import Application
 
 
+
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
-    global fixture                      # объявляем, что собираемся пользоваться этой глобальной переменной
+    global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:                 #  это условие выполняется перед вызовом 1й тестовой ф-ии
-        fixture = Application(browser=browser, base_url=base_url)          # инициализируем фикстуру
-
-    else:                     # теперь проверяем, не испортилась ли фикстура
-        if not fixture.is_valid():  # если фикстура валидная, то делать ничего не нужно, поэтому ставим NOT чтобы задать дальнейшие шаги.
-            fixture = Application(browser=browser, base_url=base_url)  # т.е создаем новую фикстуру для продолжения тестов
-    fixture.session.ensure_login(username="admin", password="secret")  # вообще лучше зд НЕ хранить пароль и ук его при запуске
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -32,6 +34,6 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--target", action="store", default="target.json")
 
 
