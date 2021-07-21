90d20e5dfee9d2808edb7e077f5f7949e9706a13
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-07-20 17:05:32 -0700

    Сделано задание №16 Тесты выплнялись также в Хроме и Эдже

--- conftest.py
+++ conftest.py
@@ -8,14 +8,15 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture                      # объявляем, что собираемся пользоваться этой глобальной переменной
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:                 #  это условие выполняется перед вызовом 1й тестовой ф-ии
-        browser = request.config.getoption("--browser")
-        fixture = Application(browser=browser)          # инициализируем фикстуру
+        fixture = Application(browser=browser, base_url=base_url)          # инициализируем фикстуру
 
     else:                     # теперь проверяем, не испортилась ли фикстура
         if not fixture.is_valid():  # если фикстура валидная, то делать ничего не нужно, поэтому ставим NOT чтобы задать дальнейшие шаги.
-            fixture = Application()  # т.е создаем новую фикстуру для продолжения тестов
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)  # т.е создаем новую фикстуру для продолжения тестов
+    fixture.session.ensure_login(username="admin", password="secret")  # вообще лучше зд НЕ хранить пароль и ук его при запуске
     return fixture
 
 
@@ -31,6 +32,6 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
 
 
--- fixture/application.py
+++ fixture/application.py
@@ -6,13 +6,15 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser="firefox"):     # запуск браузера через этот конструктор
+    def __init__(self, browser, base_url):     # запуск браузера через этот конструктор
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
             self.wd = webdriver.Chrome()
         elif browser == "edge":
             self.wd = webdriver.Edge()
+        elif browser == "opera":
+            self.wd = webdriver.Opera()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
         # self.wd.implicitly_wait(3)
@@ -20,6 +22,7 @@ class Application:
                                            # это даст возможность в одном помощнике обращаться к др помощникам
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -33,7 +36,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("/index.php"):
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
 
     def destroy(self):
