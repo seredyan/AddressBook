70926f59394c245a800e94f0b1960727d7b504ab
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-07-22 12:14:37 -0700

    Задание №18

---
+++ data/contacts.py
@@ -0,0 +1,13 @@
+
+
+
+# фиксированные данные для отладки тестов
+
+from model.contact import Contact
+
+testdata = [
+    Contact(name="name1", lastname="lastname1", address="testaddress", landline="1111", mobile="2222", workphone="3333",
+            email="test@ya.ru", email2="test@gmail.com", email3="test@icloud.com"),
+    Contact(name="NAME", lastname="LASTNAME", address="TESTADDRESS", landline="123", mobile="456", workphone="789",
+            email="TEST2@ya.ru", email2="TEST2@gmail.com", email3="TEST2@icloud.com")
+]
