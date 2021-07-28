aa28d3d68a905291de2e85ac460b5d43b7a3aafc
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-07-27 20:01:37 -0700

    Повторил действия роликов 7_8
    1) добавил в orm.py методы проверки с БД контактов, находящихся в определенной группе @db_session def get_contacts_in_group
    2) добавил в orm.py методы проверки с БД контактов, не находящихся в определенной группе @db_session get_contacts_not_in_group
    3) и добавил соотв варианты для этих методов в файл check_db_connection.py

--- check_db_connection.py
+++ check_db_connection.py
@@ -2,6 +2,7 @@
 
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 dbase = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
@@ -18,12 +19,33 @@ dbase = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="
 
 
 
-# db contacts connection
-try:
-    contacts = dbase.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
-finally:
-    pass   #dbase.destroy()
+# # db contacts connection
+# try:
+#     contacts = dbase.get_contact_list()
+#     for contact in contacts:
+#         print(contact)
+#     print(len(contacts))
+# finally:
+#     pass   #dbase.destroy()
 
+
+
+#
+# # db contacts_in_group  connection
+# try:
+#     l = dbase.get_contacts_in_group(Group(id="1003"))
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#     pass   # dbase.destroy()   # orm фвтоматически закроет соединение с БД
+
+
+# db contacts_in_group  connection
+try:
+    l = dbase.get_contacts_not_in_group(Group(id="1003"))  # выдать все контакты, не входящие в эту группу
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
+    pass   # dbase.destroy()   # orm фвтоматически закроет соединение с БД
--- fixture/orm.py
+++ fixture/orm.py
@@ -15,6 +15,8 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
+
 
 
 
@@ -24,6 +26,8 @@ class ORMFixture:
         name = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
+
 
 
     # выше -структура таблиц
@@ -56,13 +60,24 @@ class ORMFixture:
 
     @db_session   # для групп
     def get_group_list(self):
-        return self.convert_groups_to_model(select (g for g in ORMFixture.ORMGroup))
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
 
 
     @db_session  # для групп
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
 
+    @db_session
+    def get_contacts_in_group(self, group):  # зд group - объект модельного типа
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]   #  извлекаем группу с заданным id
+        return self.convert_contacts_to_model(orm_group.contacts)  # преобразуем объекты типа orm_group.contacts в модельный объекты
+
+
+    @db_session
+    def get_contacts_not_in_group(self, group):  # зд group - объект модельного типа
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
+        # выбираем все контакты, у которых список групп не содержит заданную группу (если контакт связан с группой, то он в эту выборку не попадет)
 
 
 
