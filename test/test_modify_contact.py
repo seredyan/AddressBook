6267596335e6358a703671f6265367203ed0863a
1977u <testdaxx@gmail.com>
2021-08-22 19:26:06 -0700

    Переделал задание 14 чтобы были проверки всех полей в редактируемых контактах. Добавлены знаки в фукцию clear()

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,7 +5,7 @@ from random import randrange
 
 def test_modify_contact_address(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="test"))
+        app.contact.create(Contact(name="NAME", lastname='LASTNAME', address='ADDRESS', landline='11', mobile='22', workphone='33', email='1@mail.ru', email2='2@gmail.com', email3='3@ya.ru'))
     old_contacts = app.contact.get_contact_list_split()
     index = randrange(len(old_contacts))
     modified_contact = Contact(address="Address EDITED")
@@ -14,13 +14,14 @@ def test_modify_contact_address(app):
 
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list_split()
-    # # #old_contacts[0] = modified_contact  ?? в отличие от теста удаления контакта эта строчка не нужна, тк ИМЯ контакта НЕ менял!!??
+    old_contacts[index].address = modified_contact.address
+    print('old: ', old_contacts, 'new: ', new_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_contact_phone(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="test"))
+        app.contact.create(Contact(name="NAME", lastname='LASTNAME', address='ADDRESS', landline='11', mobile='22', workphone='33', email='1@mail.ru', email2='2@gmail.com', email3='3@ya.ru'))
     old_contacts = app.contact.get_contact_list_split()
     index = randrange(len(old_contacts))
     modified_contact = Contact(mobile="cell#edited")
@@ -29,27 +30,31 @@ def test_modify_contact_phone(app):
 
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list_split()
+    old_contacts[index].mobile = modified_contact.mobile
+    print('old: ', old_contacts, 'new: ', new_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_contact_email(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="test"))
+        app.contact.create(Contact(name="NAME", lastname='LASTNAME', address='ADDRESS', landline='11', mobile='22', workphone='33', email='1@mail.ru', email2='2@gmail.com', email3='3@ya.ru'))
     old_contacts = app.contact.get_contact_list_split()
     index = randrange(len(old_contacts))
-    modified_contact = Contact(email="EDITED@anymail.com")
+    modified_contact = Contact(email="EDITED@somemail.com")
     modified_contact.id = old_contacts[index].id
     app.contact.modify_contact_by_index(index, modified_contact)
 
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list_split()
+    old_contacts[index].email = modified_contact.email
+    print('old: ', old_contacts, 'new: ', new_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
 def test_modify_contact_landline(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="test"))
+        app.contact.create(Contact(name="NAME", lastname='LASTNAME', address='ADDRESS', landline='11', mobile='22', workphone='33', email='1@mail.ru', email2='2@gmail.com', email3='3@ya.ru'))
     old_contacts = app.contact.get_contact_list_split()
     index = randrange(len(old_contacts))
     modified_contact = Contact(landline="Landline#edited")
@@ -57,7 +62,8 @@ def test_modify_contact_landline(app):
     app.contact.modify_contact_by_index(index, modified_contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list_split()
-    #old_contacts[index] = modified_contact  # ??? не нужно присвоение, потому что зд ИМЯ группы НЕ менял!!  ???
+    old_contacts[index].email = modified_contact.email
+    print('old: ', old_contacts, 'new: ', new_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
--- test/test_phones.py
+++ test/test_phones.py
@@ -67,4 +67,4 @@ def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
 
 def clear(s):
-    return re.sub("[() -]", "", s) # убираем в телефонах круглые скобки, пробелы и тире(чтобы при проверке исключить разные формы ввода номеров)
+    return re.sub("[() -+.]", "", s) # убираем в телефонах круглые скобки, пробелы и тире(чтобы при проверке исключить разные формы ввода номеров)
