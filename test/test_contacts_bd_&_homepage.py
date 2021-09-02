eb7beab76e6195ab4ac742e9a668568607c23b37
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-09-02 12:06:14 -0700

    Задание 21.
    test_all_contacts_on_home_page_to_dbase():
    - добавил сравнение количества контактов
    - убрал лишнее sorted для контактов из БД

--- test/test_contacts_bd_&_homepage.py
+++ test/test_contacts_bd_&_homepage.py
@@ -14,5 +14,6 @@ def test_all_contacts_on_home_page_to_dbase(app, db):
     print('количество в БД: ', len(all_contacts), 'количесво на гл странице: ', len(contacts_from_home_page))
     print('DB: ', all_contacts, 'homepage: ', contacts_from_home_page)
 
+    assert len(all_contacts) == len(contacts_from_home_page)
+    assert all_contacts == sorted(contacts_from_home_page, key=Contact.id_or_max)
 
-    assert sorted(all_contacts, key=Contact.id_or_max) == sorted(contacts_from_home_page, key=Contact.id_or_max)
